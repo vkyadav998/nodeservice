ec20c293e5d78442d87653b7110728f5143833ec
Vipin Yadav <vipinyadav@Vipins-MacBook-Pro.local>
2022-09-30 23:16:15 +0530

    added formconfig

---
+++ extfolder/conf.js
@@ -0,0 +1,126 @@
+const investment = {
+    "partnerDetails": {
+      "partnerName": "Test",
+      "partnerEmail": "t@t.com",
+      "partnerMobile": "6999912345"
+    },
+    "customerName": "Test",
+    "sumAssured": "4 Lakhs",
+    "premiumWithTax": "0",
+    "premium": "3,400",
+    "tax": "34,225",
+    "paymentFrequency": "Monthly",
+    "lifePremiumRequest": {
+      "dateOfBirth": 1093977000000,
+      "entryAge": 18,
+      "maturityAge": 26,
+      "gender": "M",
+      "premium": 3400,
+      "policyTerm": 8,
+      "premiumPaymentTerm": 8,
+      "investmentTermCode": "medium",
+      "paymentFrequency": 1,
+      "minIncome": 400000,
+      "maxIncome": 400000,
+      "incomeBracketCode": "",
+      "benifitCalculationRate": 8,
+      "categories": [
+        "investment"
+      ],
+      "investmentRisk": "high",
+      "currency": "INR"
+    },
+    "lifePremiumResponse": {
+      "resultId": "3aea6a5ad9d4aa5f7e304194ef758338",
+      "policyType": "TRADITIONAL",
+      "stayInvestedValue": "3 years",
+      "insurerCode": "MAXLIFELI",
+      "productCode": "P80",
+      "productName": "Smart Wealth Plan",
+      "option": "Short Term Income",
+      "optionCode": 2,
+      "productUIN": "104N116V07",
+      "tmPlanId": "",
+      "policyTerm": 9,
+      "paymentFrequency": 1,
+      "premiumPaymentTerm": 8,
+      "score": 1,
+      "category": "investment",
+      "premium": 3400,
+      "premiumAnnual": 40800,
+      "tax": 34225,
+      "taxAnnual": -34225,
+      "premiumWithTax": 0,
+      "sumAssured": 0,
+      "deathBenefitTotal": 415556,
+      "deathBenefitGuaranteed": 415556,
+      "totalBenefit": 415556,
+      "survivalBenefitTotal": 500144,
+      "survivalBenefitGuaranteed": 500144,
+      "survivalBenefitNonGuaranteed": 415556,
+      "taxSavingAmount": 170,
+      "status": "SUCCESS",
+      "companyDetails": {
+        "insurerId": "MAXLIFELI",
+        "insurerName": "Max Life Insurance",
+        "logo": "MAXLIFELI.jpg",
+        "companyDetails": "Max Life Insurance Company Limited is a Joint Venture between Max Financial Services Limited and Axis Bank Limited. Max Financial Services Ltd. is a part of the Max Group, an Indian multi business corporation. Max Life has a total claims paid ratio of 99.35% as per the annual audited financials for the FY 20-21. Max Life offers comprehensive protection and long-term savings life insurance solutions, through its multichannel distribution including agency and third party distribution partners.",
+        "speedOfClaimSettlement": {
+          "OneMonth": "99.93",
+          "OneToThreeMonths": "0.13",
+          "ThreeMonthsPlus": "0.02"
+        },
+        "speedOfClaimSettlementSummary": "99.93",
+        "insurerCode": "MAXLIFELI",
+        "contactDetails": {
+          "Telephone": "1860 120 5577",
+          "Address": "419, Bhai Mohan Singh Nagar, Railmajra, Tehsil Balachaur, District Nawanshahr, Punjab -144 533."
+        },
+        "lifeCompanyDetails": {
+          "claimRatios": {
+            "Claims paid in < 3 months": "",
+            "Claims paid < 1 year": "",
+            "Claims settled ratio (2016-17)": "",
+            "Claims paid < 6 months": "",
+            "Claims paid > 3 months": "",
+            "Claims rejected (2016-17)": "",
+            "Claims paid in < 30 days": ""
+          },
+          "urlClaimForm": "",
+          "solvencyRatio": "207%",
+          "claimSettlementRatio": "99.35%"
+        }
+      },
+      "tags": [
+        "Online"
+      ],
+      "maturityBenefits": [
+        "Maturity Benefit = Guaranteed Income benefit for payout period"
+      ],
+      "planType": "Non-participating",
+      "riderList": [],
+      "showRider": false,
+      "showRiderPremium": true,
+      "biProvider": ""
+    },
+    "addOns": [],
+    "keyFeatures": [
+      {
+        "title": "Fully guaranteed benefits to help you save for your milestones"
+      },
+      {
+        "title": "Comprehensive death benefit in case of death"
+      },
+      {
+        "title": "Choice of Income Payout Frequency"
+      },
+      {
+        "title": "Policy loans available subject to maximum of 50% of surrender value during policy term"
+      },
+      {
+        "title": "Tax benefits as per prevailing tax laws"
+      }
+    ],
+    "exclusions": [],
+    "brochureUrl": "https://catalog.mintpro.in/LIFE/P80/Max_Life_SWP.pdf"
+  }
